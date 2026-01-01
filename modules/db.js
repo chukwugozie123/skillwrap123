99e6ff3c17fce3517ea6480397181edb0df25fcf
chukwugozie123 <YOUR_EMAIL_FOR_GITHUB>
2025-12-31 17:12:28 -0800

    fixed db

--- modules/db.js
+++ modules/db.js
@@ -1,13 +1,15 @@
 const { Pool } = require('pg');
+require('dotenv').config(); // Load .env variables
 
-// Configure the pool with your external Postgres connection string
+// Configure the pool using the environment variable
 const pool = new Pool({
-  connectionString: 'postgresql://skillwrap_db_user:O3D526EAYy9bnOiaQ8RzlxnUWSpx1B0P@dpg-d59utb4hg0os73cjqhi0-a.oregon-postgres.render.com/skillwrap_db',
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false // Needed for Render.com hosted Postgres
   }
 });
 
-console.log('succesfully connected to db')
-// Export the pool to use in other parts of your app
+console.log('Successfully connected to DB');
+
+// Export the pool to use in other files
 module.exports = pool;
