e38c08946096985afb9adf13c79c07b2d5dcb9eb
Aw1 <aw1@example.com>
2026-01-25 20:03:23 -0800

    SSSSSSSSSSSS

--- config/mailer.js
+++ config/mailer.js
@@ -1,26 +1,28 @@
+// const nodemailer = require("nodemailer");
+
+// const transporter = nodemailer.createTransport({
+//   service: "gmail",
+//   auth: {
+//     user: process.env.AUTH_EMAIL,
+//     pass: process.env.AUTH_EMAIL_PASSWORD,
+//   },
+// });
+
+// module.exports = transporter;
+
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.sendgrid.net",
+  port: 587,
   auth: {
-    user: process.env.AUTH_EMAIL,
-    pass: process.env.AUTH_EMAIL_PASSWORD,
+    user: "apikey",                // literally the string "apikey"
+    pass: process.env.SENDGRID_API_KEY, // your SendGrid API key
   },
 });
 
 module.exports = transporter;
 
-// how should i go immeadietly after i sign up i ask for otp or i wait tiill they ogin in then ask fo rotp 
-// but if i do it after sign up req.user woudl be undefined soo now session untill i login in...
 
-// i nneed ur advice when should we ask for otp and whcih routes are needed
 
-// and how would i get this
-// AUTH_EMAIL,
-//     pass: process.env.AUTH_EMAIL_PASSWORD
 
-// ALTER TABLE users
-// ADD COLUMN otp_hash TEXT,
-// ADD COLUMN otp_expires_at BIGINT,
-// ADD COLUMN email_verified BOOLEAN DEFAULT false;
-// ADD COLUMN mode TEXT DEFAULT NULL;
