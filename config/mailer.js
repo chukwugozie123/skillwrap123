27605dba51f27dd9d37c4c2468f1b1d7b226d033
Aw1 <aw1@example.com>
2026-07-04 13:12:33 +0100

    fixed mailer.js

--- config/mailer.js
+++ config/mailer.js
@@ -1,31 +1,29 @@
-// config/mailer.js
-require("dotenv").config();
-const { Resend } = require("resend");
+// // config/mailer.js
+// require("dotenv").config();
+// const { Resend } = require("resend");
 
-if (!process.env.RESEND_API_KEY) {
-  throw new Error("Missing RESEND_API_KEY in environment variables");
-}
+// if (!process.env.RESEND_API_KEY) {
+//   throw new Error("Missing RESEND_API_KEY in environment variables");
+// }
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// const resend = new Resend(process.env.RESEND_API_KEY);
 
-module.exports = async function sendEmail({ to, subject, text, html }) {
-  try {
-    const response = await resend.emails.send({
-       from: "Skillwrap <noreply@skillwrap.com>", // must be verified sender
-      to,
-      subject,
-      text,
-      html,
-    });
-    // console.log("📬 Email sent via Resend:", response);
-    return response;
-  } catch (err) {
-    console.error("❌ Resend send email error:", err);
-    throw err;
-  }
-};
-
-
+// module.exports = async function sendEmail({ to, subject, text, html }) {
+//   try {
+//     const response = await resend.emails.send({
+//        from: "Skillwrap <noreply@skillwrap.com>", // must be verified sender
+//       to,
+//       subject,
+//       text,
+//       html,
+//     });
+//     // console.log("📬 Email sent via Resend:", response);
+//     return response;
+//   } catch (err) {
+//     console.error("❌ Resend send email error:", err);
+//     throw err;
+//   }
+// };
 
 
 
@@ -36,17 +34,19 @@ module.exports = async function sendEmail({ to, subject, text, html }) {
 
 
 
-// // const nodemailer = require("nodemailer");
 
-// // const transporter = nodemailer.createTransport({
-// //   service: "gmail",
-// //   auth: {
-// //     user: process.env.SENDGRID_SENDER_EMAIL,
-// //     pass: process.env.AUTH_EMAIL_PASSWORD,
-// //   },
-// // });
 
-// // module.exports = transporter;
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.SENDGRID_SENDER_EMAIL,
+    pass: process.env.AUTH_EMAIL_PASSWORD,
+  },
+});
+
+module.exports = transporter;
 
 
 
